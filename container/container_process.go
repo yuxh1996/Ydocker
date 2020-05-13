70de8a10d6e1b78d7d6e2591a9e4fde370379d59
yuxh <yuxh1996@gmail.com>
2020-05-13 23:35:21 +0800

    update

--- container/container_process.go
+++ container/container_process.go
@@ -7,9 +7,14 @@ import (
 	"syscall"
 )
 
-func NewParentProcess(ttl bool, command string) *exec.Cmd {
-	args := []string{"init", command}
-	cmd := exec.Command("/proc/self/exe", args...)
+func NewParentProcess(ttl bool) (*exec.Cmd, *os.File) {
+	readPipe, writePipe, err := NewPipe()
+	if err != nil {
+		log.Errorf("New pipe error %v", err)
+		return nil, nil
+	}
+
+	cmd := exec.Command("/proc/self/exe", "init")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
 	}
@@ -18,8 +23,15 @@ func NewParentProcess(ttl bool, command string) *exec.Cmd {
 		cmd.Stdout = os.Stdout
 		cmd.Stdout = os.Stderr
 	}
-	fmt.Println("hello")
 
-	return cmd
+	cmd.ExtraFiles = []*os.File{readPipe}
+	return cmd, writePipe
+}
 
+func NewPipe() (*os.File, *os.File, error) {
+	read, write, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	return read, write, nil
 }
--- run.go
+++ run.go
@@ -7,8 +7,13 @@ import (
 	"github.com/yuxh1996/Ydocker/container"
 )
 
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, cmdArr []string, res *subsystems.ResourceConfig) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
+
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
